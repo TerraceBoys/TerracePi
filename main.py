74a8d0156046282dda2257a347423fee1b6babc1
Brian Cox <brian5021@verizon.net>
2015-05-19 13:20:50 -0400

    Put parsing and population into a loop so it will continually refresh and handle internet disconnects

---
+++ main.py
@@ -0,0 +1,70 @@
+__author__ = 'Terrace Boiz'
+
+
+import urllib.request
+import json
+import time
+import os
+from collections import defaultdict
+
+train_url = 'http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-rcmnl&format=json'
+schedule = defaultdict(list)
+
+def main():
+    try:
+        while True:
+            print("Roxbury Crossing Schedule\n\n")
+            popDict()
+            popNorth()
+            popSouth()
+            schedule.clear()
+            # displayTime()
+            time.sleep(15)
+    except:
+        print("Load Error, Trying Again")
+        time.sleep(15)
+        main()
+
+
+
+#Dict of all times
+
+
+#test 3 with defaultdict entry
+def popDict():
+    response = urllib.request.urlopen(train_url)
+    train_data = json.loads(response.read().decode())
+    print ("Roxbury Crossing Train Schedule\n\n")
+    for x in range (len(train_data['mode']) - 1):
+        if (train_data['mode'][x]['mode_name'] == 'Subway'):
+            for y in range (len(train_data['mode'][x]['route'][0]['direction'])):
+                for z in range (len(train_data['mode'][x]['route'][0]['direction'][y]['trip'])):
+                    schedule[train_data['mode'][x]['route'][0]['direction'][y]['direction_name']].append(
+                        int(train_data['mode'][x]['route'][0]['direction'][y]['trip'][z]['pre_away']))
+
+
+
+def popNorth():
+    print("Northbound (Oak Grove)")
+    for x in range (len(schedule['Northbound'])):
+        if (x == 0):
+            m, s = divmod(schedule['Northbound'][x], 60)
+            print ("Next Train: %02d:%02d" % (m, s))
+        else:
+            m, s = divmod(schedule['Northbound'][x], 60)
+            print ("%02d:%02d" % (m, s))
+    print("\n")
+
+def popSouth():
+    print("Southbound (Forrest Hills)")
+    for x in range (len(schedule['Southbound'])):
+        if (x == 0):
+            m, s = divmod(schedule['Southbound'][x], 60)
+            print ("Next Train: %02d:%02d" % (m, s))
+        else:
+            m, s = divmod(schedule['Southbound'][x], 60)
+            print ("%02d:%02d" % (m, s))
+
+
+if __name__ == "__main__":
+    main()
