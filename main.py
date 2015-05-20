b3241a96d5de3b783e934b7cc4f8109eb94d2571
Brian Cox <brian5021@verizon.net>
2015-05-20 10:44:44 -0400

    Added multi-threading so I could run the main file of each api

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 __author__ = 'Terrace Boiz'
 
 import mbtaJsonParse
+import Weather
+import queue
+import threading
+
+files = [mbtaJsonParse, Weather]
+q = queue.Queue()
+
+def runMain(q, file):
+    q.put(file.main())
 
 if __name__ == "__main__":
-    mbtaJsonParse.main()
+    for f in files:
+        t = threading.Thread(target=runMain, args = (q, f))
+        t.daemon = True
+        t.start()
+
+    s = q.get()
+    print(s)
+
+
+
 
