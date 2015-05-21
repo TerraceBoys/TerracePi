82c9b997564310220233e1edd50e8b7e1766ff76
Brian Cox <brian5021@verizon.net>
2015-05-21 03:10:43 -0400

    Added text alerts

--- mbtaJsonParse.py
+++ mbtaJsonParse.py
@@ -4,6 +4,8 @@ __author__ = 'Terrace Boiz'
 import urllib
 import json
 import time
+import datetime
+import sendSMS
 import os
 import mbtaTimeDisplay
 from collections import defaultdict
@@ -13,6 +15,7 @@ access_key = 'MpYsZaqkKkG6p8WOeKHLqA'
 train_url = 'http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=' + access_key + '&stop=place-rcmnl&format=json'
 schedule = defaultdict(list)
 
+
 def main():
     try:
         while True:
@@ -20,12 +23,13 @@ def main():
             popDict()
             mbtaTimeDisplay.popNorth()
             mbtaTimeDisplay.popSouth()
+            checkAlert()
+            rayAlert()
             schedule.clear()
-            # displayTime()
             print spacer
             time.sleep(15)
     except:
-        print "Load Error, Trying Again"
+        print "Load Error mbta, Trying Again"
         time.sleep(15)
         main()
 
@@ -44,4 +48,29 @@ def popDict():
                     schedule[train_data['mode'][x]['route'][0]['direction'][y]['direction_name']].append(
                         int(train_data['mode'][x]['route'][0]['direction'][y]['trip'][z]['pre_away']))
 
+
+def run_once(f):
+    def wrapper():
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f()
+    wrapper.has_run = False
+    return wrapper
+
+@run_once
+def checkAlert():
+    if (datetime.datetime.now().time() >= datetime.time(hour=9, minute=30) and
+            (schedule['Northbound'][0] < 220) and (schedule['Northbound'][0] > 180)):
+        msg = 'Time To Leave bro'
+        to = [sendSMS.branden, sendSMS.brian]
+        sendSMS.send(msg, to)
+
+
+@run_once
+def rayAlert():
+    if (datetime.datetime.now().time() >= datetime.time(hour=7, minute=40) and
+            (schedule['Northbound'][0] < 220) and (schedule['Northbound'][0] > 180)):
+        msg = 'Time To Leave bro'
+        to = [sendSMS.ray]
+        sendSMS.send(msg, to)
 
--- sendSMS.py
+++ sendSMS.py
@@ -1,2 +1,27 @@
 __author__ = 'Terrace Boiz'
 
+import smtplib
+import time
+import datetime
+
+username = 'terraceraspberrySMS'
+password = 'TerraceRaspberryPi'
+fromaddr = 'terraceraspberrySMS@gmail.com'
+brian  = '5086889360@vtext.com'
+branden = '6039655776@vtext.com'
+ray = '6318977618@vtext.com'
+
+
+
+
+def send(msg, toaddr):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username, password)
+    except:
+        print("Error logging in to email client. Trying again")
+        send(msg, toaddr)
+    for t in toaddr:
+        server.sendmail(fromaddr, t, msg)
+    server.quit()
