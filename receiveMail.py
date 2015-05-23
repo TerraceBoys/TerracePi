5fdd68bfc3cec535ada476d7d0e146dd9f5baa69
Branden Rodgers <branden@rodgersworld.com>
2015-05-23 15:30:59 -0400

    email receive updates

--- receiveMail.py
+++ receiveMail.py
@@ -3,31 +3,51 @@ __author__ = 'branden'
 
 import imaplib
 import email
+import time
+import sendSMS
 
-mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login('terraceraspberrySMS@gmail.com', 'TerraceRaspberryPi')
-mail.list()
-# Out: list of "folders" aka labels in gmail.
-mail.select("inbox") # connect to inbox.
-
-result, data = mail.search(None, '(HEADER Subject "alert" UNSEEN)')
+username = 'terraceraspberrySMS@gmail.com'
+password = 'TerraceRaspberryPi'
 
 
-ids = result[len(result)-1] # data is a list.
-id_list = ids.split() # ids is a space separated string
-oldest_email_id = id_list[-1] # get the latest
+def main():
+    try:
+        while True:
+            handleMail()
+    except:
+        print "No new mail"
+        time.sleep(15)
+        main()
 
-result, data = mail.fetch(oldest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
 
-raw_email = result[0][1]# here's the body, which is raw text of the whole email
-# including headers and alternate payloads
+def handleMail():
+    global fromAddr
+    global emailBody
+    fromAddr = ""
+    emailBody = ""
 
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(username, password)
+    mail.list()
+    mail.select("inbox") # connect to inbox.
 
-email_message = email.message_from_string(raw_email)
+    #Read emails that are unread and contain the subject "alert"
+    result, data = mail.search(None, '(HEADER Subject "times" UNSEEN)')
 
-print email_message['To']
+    ids = data[0]                 # data is a list.
+    id_list = ids.split()         # ids is a space separated string
+    oldest_email_id = id_list[0]  # get the oldest
 
-print email_message['From']
+    result, data = mail.fetch(oldest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+
+    raw_email = data[0][1]# here's the body, which is raw text of the whole email
+
+    email_message = email.message_from_string(raw_email)
+
+    fromAddr = email_message['From']
+    emailBody = get_body(email_message)
+    print "New Email From: " + fromAddr
+    print "Body: " + emailBody
 
 
 
@@ -41,10 +61,10 @@ def get_body(email_message_instance):
                 break
     elif maintype == 'text':
         body += email_message_instance.get_payload()
+    return trim_body(body)
 
-    print trim_body(body)
 
-
+#Gets rid of email signature
 def trim_body(email_body):
     result = ""
     for letter in range (len(email_body)):
@@ -53,4 +73,6 @@ def trim_body(email_body):
         else:
             result += email_body[letter]
 
-get_body(email_message)
+
+if __name__ == "__main__":
+    main()
