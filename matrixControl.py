845f512e9578d18df25c5478cc7f6f20a250fcad
Brian Cox <brian5021@verizon.net>
2015-05-27 21:22:13 -0400

    Trying to run sample code on pi

--- matrixControl.py
+++ matrixControl.py
@@ -1,2 +1,41 @@
 __author__ = 'Terrace Boiz'
 
+#!/usr/bin/python
+
+# A more complex RGBMatrix example works with the Python Imaging Library,
+# demonstrating a few graphics primitives and image loading.
+# Note that PIL graphics do not have an immediate effect on the display --
+# image is drawn into a separate buffer, which is then copied to the matrix
+# using the SetImage() function (see examples below).
+# Requires rgbmatrix.so present in the same directory.
+
+# PIL Image module (create or load images) is explained here:
+# http://effbot.org/imagingbook/image.htm
+# PIL ImageDraw module (draw shapes to images) explained here:
+# http://effbot.org/imagingbook/imagedraw.htm
+
+import Image
+import ImageDraw
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+# Rows and chain length are both required parameters:
+matrix = Adafruit_RGBmatrix(32, 1)
+
+# Bitmap example w/graphics prims
+image = Image.new("1", (32, 32)) # Can be larger than matrix if wanted!!
+draw  = ImageDraw.Draw(image)    # Declare Draw instance before prims
+# Draw some shapes into image (no immediate effect on matrix)...
+draw.rectangle((0, 0, 31, 31), fill=0, outline=1)
+draw.line((0, 0, 31, 31), fill=1)
+draw.line((0, 31, 31, 0), fill=1)
+# Then scroll image across matrix...
+for n in range(-32, 33): # Start off top-left, move off bottom-right
+        matrix.Clear()
+        # IMPORTANT: *MUST* pass image ID, *NOT* image object!
+        matrix.SetImage(image.im.id, n, n)
+        time.sleep(0.05)
+
+
+
+
