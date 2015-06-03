f2af0ae44575302bee1be50b90471f50f78d845c
Branden Rodgers <branden@rodgersworld.com>
2015-06-02 22:32:53 -0400

    Hella changes. Now alerts work with any orange line stop

---
+++ alertHandler.py
@@ -0,0 +1,40 @@
+__author__ = 'branden'
+
+
+import time
+import sendSMS, mbtaJsonParse, People
+from collections import defaultdict
+
+
+def main():
+    try:
+        while True:
+            handleAlerts()
+            time.sleep(15)
+    except:
+        print "Error in alertHandler"
+        time.sleep(15)
+        main()
+
+
+#Handle custom alerts if they exist
+def handleAlerts():
+    tempDict = defaultdict(list)
+    for person in People.allPeople:
+        if person.alertInfo != None:
+            hrs, mins = person.alertInfo.time.split(':')
+            hrs = int(hrs)
+            mins = int(mins)
+            if (sendSMS.timeCheck(*[hrs, mins, 23, 59])):
+                mbtaJsonParse.popDict(tempDict, person.alertInfo.station)
+                for nextTrain in tempDict[person.alertInfo.direction]:
+                   if ((person.alertInfo.dist * 60) < nextTrain < ((person.alertInfo.dist * 60) + 70)):
+                        sendSMS.sendCustomSMS(person, nextTrain)
+                        print "Custom alert sent to: " + person.name
+                        person.alertInfo = None
+                        tempDict.clear()
+                        break
+
+
+if __name__ == "__main__":
+    main()
