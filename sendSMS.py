046e43cd0c868e50e5b213ef085958c8b287fb0e
Branden Rodgers <branden@rodgersworld.com>
2015-06-10 01:07:23 -0400

    fixed log sms problem

--- sendSMS.py
+++ sendSMS.py
@@ -104,7 +104,7 @@ def logSMS(name, msg):
     print p
     if p[0] == 'Windows':
         path = 'C:/Users/Brian Cox/Desktop/smsLog.txt'
-    elif p[0] == 'Linx' or p[0] == 'Linux2':
+    elif p[0] == 'Linux' or p[0] == 'Linux2':
         path = '/home/pi/Desktop/smsLog.txt'
     elif p[0] == 'Darwin':
         path = '/Users/branden/Desktop/smsLog.txt'
