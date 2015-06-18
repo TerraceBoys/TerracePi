bb681dcbb788fbfd1a6919ad8583886598959a63
Brian <brian5021@verizon.net>
2015-06-17 20:43:54 -0400

    led display changes

--- matrixControl.py
+++ matrixControl.py
@@ -25,9 +25,9 @@ from rgbmatrix import Adafruit_RGBmatrix
 
 
 matrix = Adafruit_RGBmatrix(32, 2)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSerif.ttf",12)
+font = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf",8)
 message = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSerif.ttf",22)
-train = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf",10)
+train = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf",11)
 weather = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf",10)
 pending_Text = []
 
@@ -36,15 +36,20 @@ def main():
     if (len(pending_Text) == 0):
     	image = Image.new("RGB", (64, 32)) # Can be larger than matrix iff wanted!!
     	draw  = ImageDraw.Draw(image)    # Declare Draw instance before prims
-    	draw.text((4,-2), "Trains", fill="blue", font=font)
-    	draw.line((4,10,34,10), fill="white")
+        draw.text((2,1), "1 TERRACE", font=font, fill="white")
+    	draw.line((0,0,63,0), fill="#000070")
+        draw.line((0,31,63,31), fill="#000070")
+        draw.line((63,1,63,30), fill="#000070")
+        draw.line((40,1,40,30), fill="#000070")
+        draw.line((0,1,0,30), fill="#000070")
+        draw.line((1,9,39,9), fill="#000070")
     	trainDisplay()
     	weatherDisplay()
     	image2 = Image.open("rain")
     	image2.load()
     	matrix.Clear()
     	matrix.SetImage(image.im.id,0,0)
-    	matrix.SetImage(image2.im.id,42,13)
+    	matrix.SetImage(image2.im.id,44,13)
     else:
         image = Image.new("RGB", (len(pending_Text[0]) * 10, 32)) # Can be larger than matrix iff wanted!!
     	draw  = ImageDraw.Draw(image)    # Declare Draw instance before prims
@@ -60,19 +65,19 @@ def trainDisplay():
     try:
         global draw
         train1, color1, train2, color2 = mbtaTimeDisplay.panelTrain(mbtaJsonParse.schedule)
-        draw.text((5,10), train1, font=train, fill=color1)
-        draw.text((5,20), train2, font=train, fill=color2)
+        draw.text((4,10), train1, font=train, fill=color1)
+        draw.text((4,19), train2, font=train, fill=color2)
     except:
-        draw.text((5,10), "No", font=train, fill="red")
-        draw.text((5,20), "Trains", font=train, fill="red")
+        draw.text((4,10), "No", font=train, fill="red")
+        draw.text((4,19), "Trains", font=train, fill="red")
 
 def weatherDisplay():
     try:
         global draw
         currentWeather, weathercolor = Weather.weatherPanel()
-        draw.text((44, 3), currentWeather, font=weather, fill=weathercolor)
+        draw.text((46, 3), currentWeather, font=weather, fill=weathercolor)
     except Exception as e:
-        # draw.text((44, 3), "NO", font=weather, fill="red")
+        # draw.text((46, 3), "NO", font=weather, fill="red")
         time.sleep(1)
         weatherDisplay()
         
