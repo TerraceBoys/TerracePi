24927cd28a8500d1d1a7fc924973b98d49fab5b7
Branden Rodgers <branden@rodgersworld.com>
2015-06-17 21:37:49 -0400

    reverted

--- main.py
+++ main.py
@@ -3,24 +3,18 @@ __author__ = 'Terrace Boiz'
 import mbtaJsonParse
 import Weather
 import receiveMail
-import baseball
 import alertHandler
 import Queue
 import threading
 
-standard = [mbtaJsonParse, Weather, receiveMail, alertHandler]
-baseball = [baseball, alertHandler, receiveMail]
-
-pi_mode = standard
-
+files = [mbtaJsonParse, Weather, receiveMail, alertHandler]
 q = Queue.Queue()
 
 def runMain(q, file):
     q.put(file.main())
 
 if __name__ == "__main__":
-
-    for f in standard:
+    for f in files:
         t = threading.Thread(target=runMain, args = (q, f))
         t.daemon = True
         t.start()
