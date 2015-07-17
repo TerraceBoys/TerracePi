ac3eef2690e1df5400a431d076f7a36c06d13523
Brian Cox <bcox5021@gmail.com>
2015-07-16 23:55:57 -0400

    Updated Brian time in the morning and what days of the week and added mark to daily alerts

--- People.py
+++ People.py
@@ -32,23 +32,24 @@ weekdays = [0,1,2,3,4]
 summerWeekdays = [0,1,2,3]
 
 brandenDaily = dailyAlert('Roxbury', 'Northbound', [9,30,10,0], summerWeekdays, 4)
-brianDaily = dailyAlert('Roxbury', 'Northbound', [9,30,10,0], summerWeekdays, 4)
+brianDaily = dailyAlert('Roxbury', 'Northbound', [8,20,10,0], weekdays, 4)
 rayDaily = dailyAlert('Roxbury', 'Northbound', [7,50,8,30], weekdays, 4)
+markDaily = dailyAlert('Roxbury', 'Northbound', [7,50,8,30], weekdays, 4)
 
 ########  People for custom and daily alerts #######################
 
 branden = Person('Branden', '6039655776@vtext.com', brandenDaily)
 brian = Person('Brian', '5086889360@vtext.com', brianDaily)
 ray = Person('Raymond', '6318977618@txt.att.net', rayDaily)
+mark = Person('Mark', '2018350105@vtext.com', markDaily)
 
-allPeople = [branden, brian, ray]
+allPeople = [branden, brian, ray, mark]
 
 
 ########  People just for insults ##############
 
 corinne = Person('Corinne', '7746446908@tmomail.net')
 allison = Person('Allison', '8608037963@vtext.com')
-mark = Person('Mark', '2018350105@vtext.com')
 jj = Person('JJ', '2153017800@vtext.com')
 allie = Person('Allie', '2019650132@txt.att.net')
 
