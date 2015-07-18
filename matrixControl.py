685af3cf243f0762565d25d5c4264acfafad57e2
Mark <m.millstein1234@gmail.com>
2015-07-18 17:17:08 -0400

    indent

--- matrixControl.py
+++ matrixControl.py
@@ -57,12 +57,12 @@ def main():
         image = Image.new("RGB", (len(pending_Text[0]) * 10, 32))  # Can be larger than matrix iff wanted!!
         draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
         draw.text((0, 0), pending_Text[0], fill="white", font=message)
-        pending_Text.pop(0)
         for n in range(64, -image.size[0], -1):
             matrix.Clear()
             matrix.SetImage(image.im.id, n, 0)
             time.sleep(0.035)
-    main()
+        pending_Text.pop(0)
+        main()
 
 
 def train_display():
