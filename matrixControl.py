b901c74cd4dfe1207004bcbfddee6746b2f9f1ea
Mark <m.millstein1234@gmail.com>
2015-07-18 17:14:47 -0400

    Moved loop

--- matrixControl.py
+++ matrixControl.py
@@ -58,10 +58,10 @@ def main():
         draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
         draw.text((0, 0), pending_Text[0], fill="white", font=message)
         pending_Text.pop(0)
-    for n in range(64, -image.size[0], -1):
-        matrix.Clear()
-        matrix.SetImage(image.im.id, n, 0)
-        time.sleep(0.035)
+        for n in range(64, -image.size[0], -1):
+            matrix.Clear()
+            matrix.SetImage(image.im.id, n, 0)
+            time.sleep(0.035)
     main()
 
 
