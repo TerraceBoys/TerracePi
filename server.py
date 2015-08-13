b53dd734a890e34d9aee4ba40463ac242dd1f1a9
Brian Cox <bcox5021@gmail.com>
2015-08-12 23:10:52 -0400

    Added starter game state and get and post methods

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 __author__ = 'Mark'
+import json
 
-from flask import Flask
+from flask import Flask, jsonify, request
+import state
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Fuck!"
 
+stateNow = state.State(1, 5, 6)
+
+@app.route("/raspberry/pi/state", methods=['GET'])
+def get_state():
+    return json.dumps(stateNow.__dict__)        
+
+@app.route("/raspberry/pi/state", methods=['POST'])
+def post_state():
+    if not request.json:
+        abort(400)
+    global stateNow
+    stateNow = state.State(request.json['id'], request.json['score1'], request.json['score2'])
+    return json.dumps(stateNow.__dict__) 
 
 def main():
     app.run(host='0.0.0.0', port=8080)
-
---
+++ state.py
@@ -0,0 +1,6 @@
+class State:
+    def __init__(self, id, score1, score2):
+        self.id = id
+        self.score1 = score1
+        self.score2 = score2
+        
