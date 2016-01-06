336dcf0520601b7e4a07a627391923d9edf3c23d
Branden Rodgers <brodgers@nanigans.com>
2016-01-05 23:16:44 -0500

    updated display

--- matrixControl.py
+++ matrixControl.py
@@ -39,8 +39,8 @@ def main():
     if len(pending_Text) == 0:
         image = Image.new("RGB", (64, 32))  # Can be larger than matrix iff wanted!!
         draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
-        draw.rectangle((2, 19, 30, 59), fill="white")
-        draw.text((2, 19), "l TERRACE", font=font, fill="black")
+        draw.rectangle((1, 16, 35, 30), fill="purple")
+        draw.text((2, 19), "l TERRACE", font=font, fill="white")
         draw.line((0, 0, 63, 0), fill="#400080") #top
         draw.line((0, 31, 63, 31), fill="#400080") #bottom
         draw.line((63, 1, 63, 30), fill="#400080") #right
